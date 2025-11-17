e761ee4240f34347b6f61ae455494fdc95fc5850
bengamer2005 <garciabenitoleonel@gmail.com>
2025-11-17 01:23:49 -0600

    se actualiza productController para obtener datos desde req.query

--- backend/controllers/productosController.js
+++ backend/controllers/productosController.js
@@ -32,19 +32,35 @@ const newProduct = async (req, res) => {
 // obtiene todos los productos
 const allProducts = async (req, res) => {
     try {
-        // llama a todos los productos de materiales
-        const productosMateriales = await productos.findAll({
-            where: { idTipoMaterial: 1, activo: 1}
+        const page = req.query.type
+        let type
+
+        switch (page) {
+            case "materiales":
+                type = 1
+            break
+        
+            case "ferreteria":
+                type = 2
+            break
+
+            default:
+                type = 3
+            break
+        }
+
+        if(type === 3) {
+            return res.status(404).json({message: "Tipo de producto no soportado."})
+        }
+
+        // llama a todos los productos de dependiendo el tipo
+        const allProductType = await productos.findAll({
+            where: { idTipoMaterial: type, activo: 1}
         })
 
-        // llama a todos los productos de ferreteria
-        const productosFerreteria = await productos.findAll({
-            where: {idTipoMaterial: 2, activo: 1}
-        })
-
-        res.status(200).json({productosMateriales, productosFerreteria})
+        res.status(200).json({allProductType})
     } catch (error) {
-        res.status(500).json({message: "Ocurrio un error al llamar a todos los productos, error: ", error})
+        res.status(500).json({message: "Ocurrio un error al llamar a todos los productos, error: ", error: error.message})
     }
 }
 
